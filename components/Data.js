8d6c5984ce4b49e26a1a0002004036018943867c
Thomas Murzyn <murzynthomas@gmail.com>
2023-02-15 16:40:34 +0100

    comment data

--- components/Data.js
+++ components/Data.js
@@ -23,34 +23,34 @@ const data = [
       "Un site qui recense et qui permet de chercher tous les jeux vidéo, le site utilise l'api de RAWG. Il a été réalisé avec la MERN Stack c'est à dire avec les technologies Mongo DB, Express, React js et Node js.",
   },
 
-  {
-    id: 3,
-    name: "Réplique Full Stack de Vinted",
-    techno: ["Mongo DB", "Express", "React js", "Node js / Javascript"],
-    picture: vintedPicture,
-    url: "https://vinted-by-thomas.netlify.app",
-    description:
-      "Ce site est une réplique du célébre site Vinted. Il a été réalisé avec la MERN Stack c'est à dire avec les technologies Mongo DB, Express, React js et Node js.",
-  },
+  // {
+  //   id: 3,
+  //   name: "Réplique Full Stack de Vinted",
+  //   techno: ["Mongo DB", "Express", "React js", "Node js / Javascript"],
+  //   picture: vintedPicture,
+  //   url: "https://vinted-by-thomas.netlify.app",
+  //   description:
+  //     "Ce site est une réplique du célébre site Vinted. Il a été réalisé avec la MERN Stack c'est à dire avec les technologies Mongo DB, Express, React js et Node js.",
+  // },
 
-  {
-    id: 4,
-    name: "Réplique Front-end de TripAdvisor",
-    techno: ["HTML", "CSS", "Vanilla Javascript"],
-    picture: tripadvisorPicture,
-    url: "https://tripadvisor-by-thomas.netlify.app",
-    description:
-      "Ce site est une réplique Front-End de la page Paris du célèbre site TripAdvisor. Il a été réalisé en HTML CSS et Vanilla Javascript.",
-  },
+  // {
+  //   id: 4,
+  //   name: "Réplique Front-end de TripAdvisor",
+  //   techno: ["HTML", "CSS", "Vanilla Javascript"],
+  //   picture: tripadvisorPicture,
+  //   url: "https://tripadvisor-by-thomas.netlify.app",
+  //   description:
+  //     "Ce site est une réplique Front-End de la page Paris du célèbre site TripAdvisor. Il a été réalisé en HTML CSS et Vanilla Javascript.",
+  // },
 
-  {
-    id: 5,
-    name: "Marvel",
-    techno: ["HTML", "CSS", "React", "Node js", "Mongo db", "Express"],
-    picture: marvelPicture,
-    url: "https://marvel-by-tm.netlify.app",
-    description: "Un site pour chercher les characters et comics marvel.",
-  },
+  // {
+  //   id: 5,
+  //   name: "Marvel",
+  //   techno: ["HTML", "CSS", "React", "Node js", "Mongo db", "Express"],
+  //   picture: marvelPicture,
+  //   url: "https://marvel-by-tm.netlify.app",
+  //   description: "Un site pour chercher les characters et comics marvel.",
+  // },
 ];
 
 export default data;
